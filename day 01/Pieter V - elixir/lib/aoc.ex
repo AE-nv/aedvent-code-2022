f117cecc1addb9583c06bbb98801d421e2831bd0
Pieter Verlinden <pieter.verlinden@mediahuis.be>
2022-12-01 23:06:37 +0100

    pieter v day 01 part2

--- day 01/Pieter V - elixir/lib/aoc.ex
+++ day 01/Pieter V - elixir/lib/aoc.ex
@@ -1,17 +1,33 @@
 defmodule Aoc do
-  def aoc_1(input) do
-    input
+  def aoc_1_base(input) do 
+     input
     |> String.split("\r\n\r\n")
     |> Enum.map(&String.split(&1,"\r\n"))
     |> Enum.map(&Enum.map(&1,fn x -> Integer.parse(x) end))
     |> Enum.map(&Enum.map(&1,fn {num, _} -> num end))
-    |> Enum.map(&List.foldl(&1,0,fn(x,acc) -> x + acc end))
+    |> Enum.map(&Enum.reduce(&1,fn(x,acc) -> x + acc end))
+  end
+  def aoc_1_1(input) do
+    input
+    |> aoc_1_base
     |> Enum.reduce(&max/2)
   end
-  def run1 do
+  def aoc_1_2(input) do
+    input
+    |> aoc_1_base
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.reduce(fn x,acc -> x+acc end)
+  end
+  def run1_1 do
     {:ok,contents} = File.read("C:/Users/Pieter.Verlinden/Documents/AE Studio/advent of code/aoc/aoc1.input.txt")
-    IO.puts(aoc_1(contents))
+    IO.puts(aoc_1_1(contents))
+  end
+  def run1_2 do
+    {:ok,contents} = File.read("C:/Users/Pieter.Verlinden/Documents/AE Studio/advent of code/aoc/aoc1.input.txt")
+    IO.puts(aoc_1_2(contents))
   end
 end
 
-Aoc.run1()
+Aoc.run1_1()
+Aoc.run1_2()
--- day 01/Pieter V - elixir/test/aoc_test.exs
+++ day 01/Pieter V - elixir/test/aoc_test.exs
@@ -4,6 +4,7 @@ defmodule AocTest do
 
   test "aoc1 satisfies demoinput" do
     {:ok,contents} = File.read("C:/Users/Pieter.Verlinden/Documents/AE Studio/advent of code/aoc/test/aoc1.input.test.txt")
-    assert Aoc.aoc_1(contents) == 24000
+    assert Aoc.aoc_1_1(contents) == 24000
+    assert Aoc.aoc_1_2(contents) == 45000
   end
 end
