05782a530305519e8c7aaff792a901df07c088be
Glenn Dejaeger <glenn.dejaeger@fluxys.com>
2022-12-01 21:23:51 +0100

    Day1 - Glenn - improved

--- day 01/Glenn - Javascript/main.js
+++ day 01/Glenn - Javascript/main.js
@@ -1,46 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const inputFile = "input.txt";
+process();
 
-try {
-    const data = fs.readFileSync(inputFile, 'utf8');
-    const elves = process(data);
-
-    calculateMaxCalories(elves);
-} catch (err) {
-    console.error(err);
-}
-
-function process(data) {
+function process() {
+    const data = fs.readFileSync("input.txt", "utf8");
     let elves = [];
     let calories = 0;
 
     data.split(/\r\n/).forEach(line => {
         if (line === "") {
-            console.log(`Elf carried ${calories} calories`);
             elves.push(calories);
+            elves.sort((a, b) => b - a);
             calories = 0;
         } else {
             calories += parseInt(line);
         }
     });
 
-    return elves;
-}
-
-function calculateMaxCalories(elves) {
-    let maxCaloriesSum = 0;
-
-    for (let i = 0; i < 3; i++) {
-        const maxCalories = Math.max(...elves);
-        const maxCaloriesIndex = elves.indexOf(maxCalories);
-
-        maxCaloriesSum += maxCalories;
-        elves.splice(maxCaloriesIndex, 1);
-
-
-        console.log(`=> Most carried calories by Elf ${maxCaloriesIndex}: ${maxCalories}`);
-    }
-
-    console.log(`=> Total carried calories by top 3 Elves: ${maxCaloriesSum}`);
+    calories = elves[0] + elves[1] + elves[2];
+    console.log(calories);
 }
