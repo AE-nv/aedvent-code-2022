89551685098aa00155574ce82704ba2d94dd0bd7
Pieter Verlinden <pieter.verlinden@mediahuis.be>
2022-12-02 19:05:08 +0100

    update-day01

--- day 01/Pieter V - elixir/aoc1.ex
+++ day 01/Pieter V - elixir/aoc1.ex
@@ -1,11 +1,11 @@
-defmodule Aoc do
+defmodule Aoc1 do
   def aoc_1_base(input) do 
      input
     |> String.split("\r\n\r\n")
     |> Enum.map(&String.split(&1,"\r\n"))
     |> Enum.map(&Enum.map(&1,fn x -> Integer.parse(x) end))
     |> Enum.map(&Enum.map(&1,fn {num, _} -> num end))
-    |> Enum.map(&Enum.reduce(&1,fn(x,acc) -> x + acc end))
+    |> Enum.map(&Enum.sum(&1))
   end
   def aoc_1_1(input) do
     input
@@ -17,7 +17,7 @@ defmodule Aoc do
     |> aoc_1_base
     |> Enum.sort(:desc)
     |> Enum.take(3)
-    |> Enum.reduce(fn x,acc -> x+acc end)
+    |> Enum.sum
   end
   def run1_1 do
     {:ok,contents} = File.read("C:/Users/Pieter.Verlinden/Documents/AE Studio/advent of code/aoc/aoc1.input.txt")
@@ -29,5 +29,5 @@ defmodule Aoc do
   end
 end
 
-Aoc.run1_1()
-Aoc.run1_2()
+Aoc1.run1_1()
+Aoc1.run1_2()
