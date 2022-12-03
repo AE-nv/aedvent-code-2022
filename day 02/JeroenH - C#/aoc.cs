cfb4106b685e68ffee50faa2a2f48a00285bbd32
jhgbrt <jeroen@haegebaert.com>
2022-12-03 11:12:50 +0100

    day 3

--- day 02/JeroenH - C#/aoc.cs
+++ day 02/JeroenH - C#/aoc.cs
@@ -1,3 +1,6 @@
+using static RPS;
+using static Result;
+
 var input = File.ReadAllLines("input.txt");
 var sw = Stopwatch.StartNew();
 var part1 = Part1();
@@ -5,42 +8,66 @@ var part2 = Part2();
 Console.WriteLine((part1, part2, sw.Elapsed));
 int Part1() => (
     from line in input
-    let l = Convert(line[0])
-    let r = Convert(line[2])
-    select (int)r + Score(l, r)).Sum();
+    let opponentMove = ToMove(line[0])
+    let myMove = ToMove(line[2])
+    select CalculateScore(myMove, Play(opponentMove, myMove))).Sum();
 int Part2() => (
     from line in input
-    let l = Convert(line[0])
-    let r = DefineMove(l, line[2])
-    select (int)r + Score(l, r)).Sum();
-RPS Convert(char input) => input switch
+    let opponentMove = ToMove(line[0])
+    let result = ToResult(line[2])
+    let myMove = DefineMove(opponentMove, result)
+    select CalculateScore(myMove, result)).Sum();
+int CalculateScore(RPS move, Result result)
+{
+    var value = move switch
+    {
+        Rock => 1,
+        Paper => 2,
+        Scissors => 3
+    };
+    var score = result switch
+    {
+        Lose => 0,
+        Draw => 3,
+        Win => 6
+    };
+    return value + score;
+}
+
+RPS ToMove(char input) => input switch
 {
     'A' or 'X' => Rock,
     'B' or 'Y' => Paper,
     'C' or 'Z' => Scissors
 };
-int Score(RPS l, RPS r) => (l, r) switch
+Result ToResult(char input) => input switch
 {
-    (Rock, Rock) => 3,
-    (Rock, Paper) => 6,
-    (Rock, Scissors) => 0,
-    (Paper, Rock) => 0,
-    (Paper, Paper) => 3,
-    (Paper, Scissors) => 6,
-    (Scissors, Rock) => 6,
-    (Scissors, Paper) => 0,
-    (Scissors, Scissors) => 3
+    'X' => Lose,
+    'Y' => Draw,
+    'Z' => Win
 };
-RPS DefineMove(RPS opponent, char outcome) => outcome switch
+Result Play(RPS opponentMove, RPS myMove) => (opponentMove, myMove) switch
 {
-    'X' /* lose */ => opponent switch
+    (Rock, Rock) => Draw,
+    (Rock, Paper) => Win,
+    (Rock, Scissors) => Lose,
+    (Paper, Rock) => Lose,
+    (Paper, Paper) => Draw,
+    (Paper, Scissors) => Win,
+    (Scissors, Rock) => Win,
+    (Scissors, Paper) => Lose,
+    (Scissors, Scissors) => Draw
+};
+RPS DefineMove(RPS opponentMove, Result outcome) => outcome switch
+{
+    Lose => opponentMove switch
     {
         Rock => Scissors,
         Paper => Rock,
         Scissors => Paper
     },
-    'Y' => opponent,
-    'Z' /* win */ => opponent switch
+    Draw => opponentMove,
+    Win => opponentMove switch
     {
         Rock => Paper,
         Paper => Scissors,
@@ -49,7 +76,14 @@ RPS DefineMove(RPS opponent, char outcome) => outcome switch
 };
 enum RPS
 {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3
+    Rock,
+    Paper,
+    Scissors
+}
+
+enum Result
+{
+    Win,
+    Draw,
+    Lose
 }
