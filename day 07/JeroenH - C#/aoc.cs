11a0d8b81f6986a1cac2c30835365f14bec74133
jhgbrt <jeroen@haegebaert.com>
2022-12-07 11:35:20 +0100

    day 7 c#

--- day 07/JeroenH - C#/aoc.cs
+++ day 07/JeroenH - C#/aoc.cs
@@ -1,31 +1,39 @@
-var input = File.ReadAllLines("input.txt");
+var input = System.IO.File.ReadAllLines("input.txt");
+
 var Root = Directory.Parse(input);
+
 var sw = Stopwatch.StartNew();
 var part1 = Part1();
 var part2 = Part2();
 Console.WriteLine((part1, part2, sw.Elapsed));
+
 long Part1() => (
     from d in Root.AllChildren()
     where d.Size < 100000
     select d.Size).Sum();
+
 long Part2() => (
     from d in Root.AllChildren()
     where d.Size >= (Root.Size - 40000000)
     orderby d.Size
     select d).First().Size;
+
 abstract class FileSystemEntry
 {
     public FileSystemEntry(string name) => Name = name;
     public string Name { get; }
-
-    public List<FileSystemEntry> Children { get; } = new();
+    protected List<FileSystemEntry> Children { get; } = new();
     public abstract long Size { get; }
 }
 
 partial class Directory : FileSystemEntry
 {
+    private Directory(string name, Directory? parent) : base(name) => Parent = parent;
+
     static Regex FileRegex = CreateFileRegex();
+
     static Regex DirectoryRegex = CreateDirectoryRegex();
+
     public static Directory Parse(IEnumerable<string> input)
     {
         var root = new Directory("/", null);
@@ -36,8 +44,10 @@ partial class Directory : FileSystemEntry
             {
                 "$ cd" => cd.Find(line[5..]),
                 "$ ls" => cd,
-                _ when DirectoryRegex.Match(line) is { Success: true } m => cd.AddDirectory(m.Groups["name"].Value),
-                _ when FileRegex.Match(line) is { Success: true } m => cd.AddFile(m.Groups["name"].Value, long.Parse(m.Groups["size"].Value)),
+                _ when DirectoryRegex.Match(line) is { Success: true } m
+                    => cd.AddDirectory(m.Groups["name"].Value),
+                _ when FileRegex.Match(line) is { Success: true } m 
+                    => cd.AddFile(m.Groups["name"].Value, long.Parse(m.Groups["size"].Value)),
                 _ => throw new NotImplementedException()
             };
         }
@@ -56,10 +66,11 @@ partial class Directory : FileSystemEntry
         yield return this;
     }
 
-    private Directory(string name, Directory? parent) : base(name) => Parent = parent;
+
     public Directory? Parent { get; }
 
     public override long Size => Children.Sum(f => f.Size);
+
     public Directory AddFile(string name, long size)
     {
         Children.Add(new File(name, size));
@@ -77,9 +88,12 @@ partial class Directory : FileSystemEntry
         ".." => Parent ?? throw new Exception("root does not have parent"),
         _ => Children.OfType<Directory>().First(c => c.Name == name)
     };
+
     public override string ToString() => $"dir {Name}";
+
     [GeneratedRegex("^(?<size>\\d+) (?<name>.+)$", RegexOptions.Compiled)]
     private static partial Regex CreateFileRegex();
+
     [GeneratedRegex("^dir (?<name>.+)$", RegexOptions.Compiled)]
     private static partial Regex CreateDirectoryRegex();
 }
@@ -87,6 +101,7 @@ partial class Directory : FileSystemEntry
 class File : FileSystemEntry
 {
     public File(string name, long size) : base(name) => Size = size;
+
     public override long Size { get; }
 
     public override string ToString() => $"{Size} {Name}";
